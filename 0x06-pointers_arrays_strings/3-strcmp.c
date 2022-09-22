abc3b7f6daa393ed3529a65e3869f17bd74b07aa
Saibantoril <saheedolabanji@gmail.com>
2022-09-21 23:27:23 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,9 @@
  * @s1: first string
  * @s2: second string
  * return:
- * 		returns zero if s1 == s2
- * 		returns negative number if s1 < s2
- * 		returns positive number if s1 > s2
+ * returns zero if s1 == s2
+ * returns negative number if s1 < s2
+ * returns positive number if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -28,7 +28,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (s1[i] != s2[i])
 		{
-			diff =s1[i] - s2[i];
+			diff = s1[i] - s2[i];
 			break;
 		}
 		else
