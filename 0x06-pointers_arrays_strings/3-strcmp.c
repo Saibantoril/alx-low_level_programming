f161343cc6e12df9aa781109a732248ab4d3eb50
Saibantoril <saheedolabanji@gmail.com>
2022-09-22 01:19:36 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * strcmp - Function which compare two strings and
+ * _strcmp - Function which compare two strings and
  * @s1: first string
  * @s2:second string
  * Return:
