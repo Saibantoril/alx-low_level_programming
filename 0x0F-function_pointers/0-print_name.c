1e5078af8c2c1adf571e3b3dc815ea909f035da2
Saibantoril <saheedolabanji@gmail.com>
2022-10-12 08:04:31 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,9 @@
  * print_name - prints a name
  * @name: name to print
  * @f: function to print with
+ *
  * Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
