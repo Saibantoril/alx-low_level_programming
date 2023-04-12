b2b5e75bd5d50e33de2807ba60c90a364ccc8b05
Saibantoril <110996980+Saibantoril@users.noreply.github.com>
2023-04-12 15:51:57 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,20 +13,20 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t x;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-
-        if (array[i] == value)
-            return (i);
-    }
-
-    return (-1);
-}
-
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
 }
